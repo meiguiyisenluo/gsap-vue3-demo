1078a11fada7884a5229c4a14d9eb09dd032bdb8
luoyisen <1402175410@qq.com>
2025-09-28 15:16:52 +0800

    fix

--- src/views/HomeView/Index.vue
+++ src/views/HomeView/Index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div
-    v-for="(item, index) in imageList"
-    :key="index"
-    :class="`container`"
-    :ref="(el) => (containers[index] = el as HTMLDivElement)"
-    :style="{
-      backgroundImage: `url(${item.src})`,
-      height: `${lockedWindowInnerHeight}px`,
-    }"
-  >
-    <p>{{ item.text }}</p>
+  <div>
+    <div
+      v-for="(item, index) in imageList"
+      :key="index"
+      :class="`container`"
+      :ref="(el) => (containers[index] = el as HTMLDivElement)"
+      :style="{
+        backgroundImage: `url(${item.src})`,
+      }"
+    >
+      <p>{{ item.text }}</p>
+    </div>
   </div>
 </template>
 
@@ -18,37 +19,11 @@ defineOptions({
   name: 'HomeView',
 })
 
+import imageList from './imageList'
+
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
-
-import bg01 from '@/views/HomeView/assets/img/01.png'
-import bg02 from '@/views/HomeView/assets/img/02.png'
-import bg03 from '@/views/HomeView/assets/img/03.png'
-import bg04 from '@/views/HomeView/assets/img/04.png'
-
-const imageList = [
-  {
-    src: bg01,
-    from: '【哲风壁纸】夜景-夜色风光-星空',
-    text: 'Every setback is a setup for a comeback.',
-  },
-  {
-    src: bg02,
-    from: '【哲风壁纸】夜景-富士山',
-    text: 'You are stronger than you think, and braver than you believe.',
-  },
-  {
-    src: bg03,
-    from: '【哲风壁纸】富士山-富士山倒影',
-    text: 'Progress is progress, no matter how small.',
-  },
-  {
-    src: bg04,
-    from: '【哲风壁纸】森林-车站',
-    text: 'Keep going—your future self will thank you.',
-  },
-]
 
 import { onMounted } from 'vue'
 const onMountedP = new Promise((resolve) => onMounted(resolve))
@@ -61,28 +36,34 @@ const initGsap = async () => {
   for (let i = 0; i < containers.length; i++) {
     const container = containers[i]
     if (!container) continue
-    // const p = container.querySelector('p')
-    // const { height } = p!.getBoundingClientRect()
-
-    // gsap.to(p, {
-    //   ease: 'none',
-    //   y: lockedWindowInnerHeight - height,
-    //   scrollTrigger: {
-    //     trigger: container,
-    //     scrub: true,
-    //     start: i === '0' ? 'top top' : 'center bottom',
-    //     end: 'bottom top',
-    //   },
-    // })
+    if (i === 0) {
+      const p = container.querySelector('p')
+      gsap.fromTo(
+        p,
+        {
+          translateY: 0,
+        },
+        {
+          translateY: `-${lockedWindowInnerHeight / 3}px`,
+          ease: 'none',
+          scrollTrigger: {
+            trigger: container,
+            scrub: true,
+            start: 'top top',
+            end: 'bottom top',
+          },
+        },
+      )
+    }
 
     if (i !== 0) {
       gsap.fromTo(
         container,
         {
-          backgroundPositionY: `-${lockedWindowInnerHeight / 2}px`,
+          backgroundPositionY: `-${lockedWindowInnerHeight / 2.5}px`,
         },
         {
-          backgroundPositionY: `${lockedWindowInnerHeight / 2}px`,
+          backgroundPositionY: `${lockedWindowInnerHeight / 2.5}px`,
           ease: 'none',
           scrollTrigger: {
             trigger: container,
@@ -111,7 +92,7 @@ onMounted(() => {
   /* will-change: scroll-position; */
   user-select: none;
   width: 100vw;
-  /* height: 100vh; */
+  height: 100vh;
 
   background-position-x: center;
   background-size: cover;
---
+++ src/views/HomeView/imageList.ts
@@ -0,0 +1,60 @@
+
+import bg01 from '@/views/HomeView/assets/img/01.png'
+import bg02 from '@/views/HomeView/assets/img/02.png'
+import bg03 from '@/views/HomeView/assets/img/03.png'
+import bg04 from '@/views/HomeView/assets/img/04.png'
+import bg05 from '@/views/HomeView/assets/img/05.png'
+import bg06 from '@/views/HomeView/assets/img/06.png'
+import bg07 from '@/views/HomeView/assets/img/07.png'
+import bg08 from '@/views/HomeView/assets/img/08.png'
+import bg09 from '@/views/HomeView/assets/img/09.png'
+
+const imageList = [
+  {
+    src: bg01,
+    from: '【哲风壁纸】夜景-夜色风光-星空',
+    text: 'Every setback is a setup for a comeback.',
+  },
+  {
+    src: bg02,
+    from: '【哲风壁纸】夜景-富士山',
+    text: 'You are stronger than you think, and braver than you believe.',
+  },
+  {
+    src: bg03,
+    from: '【哲风壁纸】富士山-富士山倒影',
+    text: 'Progress is progress, no matter how small.',
+  },
+  {
+    src: bg04,
+    from: '【哲风壁纸】森林-车站',
+    text: 'Keep going—your future self will thank you.',
+  },
+  {
+    src: bg05,
+    from: '【哲风壁纸】清晨-雪山',
+    text: 'Your potential is endless—keep climbing.',
+  },
+  {
+    src: bg06,
+    from: '【哲风壁纸】光斑-水波纹-水面',
+    text: 'Storms make trees take deeper roots.',
+  },
+  {
+    src: bg07,
+    from: '【哲风壁纸】护眼草地-绿色山丘',
+    text: 'You don’t have to be perfect to be powerful.',
+  },
+  {
+    src: bg08,
+    from: '【哲风壁纸】丁达尔-乌云-云层',
+    text: 'Small steps every day lead to big changes.',
+  },
+  {
+    src: bg09,
+    from: '【哲风壁纸】夏天森林-大树仰拍',
+    text: 'You are not behind—you’re building momentum.',
+  },
+]
+
+export default imageList
