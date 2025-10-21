8d1f430c400d11bff59c416bae1f2b642ef9e0dd
luoyisen <1402175410@qq.com>
2025-10-21 16:32:34 +0800

    preload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,23 @@ import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import viteImagemin from 'vite-plugin-imagemin'
+import preload from "vite-plugin-preload"
+
+import path from 'node:path'
+
+const preloadPrefix = 'preload-'
+/**
+ * 判断某个文件是否在指定目录中
+ * @param filePath assetInfo.originalFileName，例如 'src/views/HomeView/assets/img/05.png'
+ * @param targetDir 目标目录，例如 'src/views/HomeView/assets/img'
+ */
+const isInDirectory=(filePath: string, targetDir: string): boolean =>{
+  const absoluteFile = path.resolve(filePath)
+  const absoluteDir = path.resolve(targetDir)
+  const relative = path.relative(absoluteDir, absoluteFile)
+  return !relative.startsWith('..') && !path.isAbsolute(relative)
+}
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -40,10 +57,29 @@ export default defineConfig({
         ],
       },
     }),
+    preload({
+      mode: 'preload',
+      shouldPreload: (chunkInfo) => {
+        return chunkInfo.fileName.startsWith(preloadPrefix)
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let target = 'assets/[name]-[hash][extname]'
+          if (isInDirectory(assetInfo.originalFileNames[0] ?? '', 'src/views/HomeView/assets/img')) {
+            target = preloadPrefix + target
+          }
+          return target
+        }
+      }
+    }
+  }
 })
